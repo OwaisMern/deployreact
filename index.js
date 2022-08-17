e97cb3bb77115d7a9b3c9e1eca9aee1b5905f050
w3schools-test <test@w3schools.com>
2022-08-18 04:30:41 +0500

    make it better

---
+++ index.js
@@ -0,0 +1,36 @@
+const port = process.env.PORT || 4000
+const express = require('express')
+var cors = require('cors')
+const app = express()
+var mysql      = require('mysql');
+var bodyParser = require('body-parser')
+app.use(cors())
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+var connection = mysql.createConnection({
+    host     : 'remotemysql.com',
+    user     : 'MgCEspKABp',
+    password : 'W3iohxgdyL',
+    database : 'MgCEspKABp'
+  });
+
+  
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.get('/check', (req, res) => {
+    connection.query('SELECT * FROM user', function (error, results, fields) {
+        if (error) throw error;
+        res.send(results)
+    });
+})
+
+app.post('/register', (req, res) => {
+    res.json(req.body)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
